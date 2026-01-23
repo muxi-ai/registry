1274b62863f3522e4bc69843fc9b19d066bd7cb7
Ran Aroussi <ran@aroussi.com>
2026-01-23 00:44:16 +0000

    feat: add link-on-hover class for enhanced link styling and interaction

--- website/app/views/profile/index.php
+++ website/app/views/profile/index.php
@@ -83,7 +83,7 @@ tiny::components()->require('FormationCard');
     </div>
 
     <div class="col-span-8 h-full">
-        <h2 class="text-3xl font-semibold font-sans! border-b pb-3 mb-4">Formations</h2>
+        <h2 class="text-2xl font-semibold font-sans! mb-2 px-0.5">Formations</h2>
 
         <?php if (empty(tiny::data()->formations)): ?>
             <!-- Empty State -->
@@ -130,7 +130,7 @@ tiny::components()->require('FormationCard');
                 </div>
             </div>
         <?php else: ?>
-            <div class="tabs w-full mb-4">
+            <div class="tabs w-full my-4">
                 <nav role="tablist" class="w-full space-x-1 bg-black/5 dark:bg-white/5">
                     <a hx-boost="true" hx-push-url="true"
                         href="<?php tiny::homeURL('/@' . tiny::data()->profile['registry_username'] . '?sort=recent'); ?>" role="tab" aria-selected="<?php echo tiny::data()->sort === 'recent' ? "true" : "false"; ?>" class="btn-ghost">
@@ -178,10 +178,38 @@ tiny::components()->require('FormationCard');
             </div>
 
 
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                <?php foreach (tiny::data()->formations as $formation): ?>
-                    <?php tiny::components()->FormationCard(formation: $formation, showStats: true); ?>
-                <?php endforeach; ?>
+            <div class="divide-y border-y px-0.5 gap-4 [&>div]:flex [&>div]:items-start [&>div]:justify-between [&>div]:py-6 &>div]:my-4">
+            <?php foreach (tiny::data()->formations as $formation): ?>
+                <div>
+                <!-- name, description, latest_version, github_stars, total_downloads, published_at -->
+                    <div>
+                        <a href="" class="text-lg link-on-hover"><?php echo $formation['name']; ?> ›</a>
+                        <div class="text-sm opacity-60 dark:opacity-50"><?php echo $formation['description']; ?></div>
+                        <div class="flex items-center opacity-60 dark:opacity-50 mt-2">
+                            <span class="border rounded-full text-[10px]! subpixel-antialiased bg-white/50 dark:bg-white/5 leading-none pt-[3px] pb-1 px-1.5 mr-4">v <?php echo htmlspecialchars($formation['latest_version']); ?></span>
+                            <span class="text-xs"><?php echo date('F j, Y', strtotime($formation['published_at'])); ?></span>
+                        </div>
+                    </div>
+
+                    <div class="text-right -mt-1.5">
+                        <ul class="divide-x text-sm flex items-center divider-x gap-x-2 p-0! my-0! -mx-px! [&>li]:m-0 [&>li]:pr-2 [&>li]:flex [&>li]:items-center [&>li]:space-x-2 [&>li>svg]:size-3.5 [&>li>span>strong]:pr-0.5 [&>li>span>strong]:font-semibold">
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" class="px-px">
+                                    <path d="M3.2 18C1.88484 17.235 1 15.8051 1 14.1674C1 12.1053 2.40285 10.3727 4.30122 9.88197C4.30041 9.83571 4.3 9.78935 4.3 9.7429C4.3 5.46661 7.74741 2 12 2C15.6211 2 18.6584 4.51348 19.4806 7.90009C21.5395 8.69955 23 10.7089 23 13.0613C23 14.8707 22.1359 16.4772 20.8 17.4862M12 13V22M12 22L15.5 18.5M12 22L8.5 18.5" stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <span><strong><?php echo number_format($formation['total_downloads']); ?></strong> Pull<?php echo tiny::data()->stats['total_downloads'] != 1 ? 's' : ''; ?></span>
+                            </li>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path fill="currentColor" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+                                </svg>
+                                <span><strong><?php echo number_format($formation['github_stars']); ?></strong> Star<?php echo tiny::data()->stats['total_stars'] != 1 ? 's' : ''; ?></span>
+                            </li>
+                        </ul>
+                        <div class="text-xs mr-2 opacity-60 dark:opacity-50 -mt-1!"><?php echo $formation['license'] ?? 'MIT License'; ?></div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
             </div>
         <?php endif; ?>
     </div>
