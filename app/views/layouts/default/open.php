1822d4ac48330d17c36604c56655ab7ede5699c5
Ran Aroussi <ran@aroussi.com>
2026-06-14 00:30:30 +0100

    chore: add 'sameAs' property to structured data in open.php

--- app/views/layouts/default/open.php
+++ app/views/layouts/default/open.php
@@ -41,7 +41,10 @@ $defaultRobots = 'noindex, nofollow';
             "@id": "https://aroussi.com/#person",
             "name": "Ran Aroussi",
             "url": "https://aroussi.com"
-        }
+        },
+        "sameAs": [
+            "https://www.wikidata.org/wiki/Q140196048"
+        ]
     }
     </script>
 
